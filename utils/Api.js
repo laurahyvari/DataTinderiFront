e407c95d4488ca4e5a3f17a691cd6ef0754ee985
laurahyvari <54351351+laurahyvari@users.noreply.github.com>
2021-02-24 11:32:07 +0200

    Revert "Axios"

--- utils/Api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const getSuggestions = async (count) => {
-  const response = await axios.get(`https://data-tinder-back.herokuapp.com/api/programs/${count}`)
-  return response.data
-}
-
-export default { getSuggestions }
