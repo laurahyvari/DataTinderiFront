5d68b677187687955e794ac576328b167a262b75
Jussi Häyhä <jussihayha@gmail.com>
2021-04-06 21:01:29 +0300

    Listaruutu hakee tiedot suoraan Herokusta, vaihto bäkkäriin myöhemmin

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,32 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import FontAwesome5 from '@expo/vector-icons/FontAwesome5'
+import firebase, { db } from "../config/Firebase";
 
 const ListItem = (props) => {
   return (
     <View style={styles.listItem}>
-      <Text style={styles.programTitle}>{props.program.title}</Text>
-      <FontAwesome5 name={'play'} size={18} style={styles.playIcon}/>
+      <Text style={styles.programTitle}>{props.program}</Text>
+      <FontAwesome5 name={'play'} size={18} style={styles.playIcon} />
     </View>
   )
 }
 
 export default function ListScreen(props) {
 
-  useEffect(() => {
-    console.log('User liked these: ', props.likedPrograms)
-  }, [])
+  const [likes, setLikes] = useState([]);
+  const user = firebase.auth().currentUser;
+  const uid = user.uid;
 
   const navigateToPlayer = () => {
     props.navigation.navigate('Player')
   }
 
+  useEffect(() => {
+    getLikes();
+    console.log(likes)
+  }, []);
+
+  const getLikes = () => {
+    db.ref("users")
+      .child(uid)
+      .child("preferences/")
+      .on("value", (snapshot) => {
+        let data = [];
+        snapshot.forEach((child) => {
+          data.push(child.val()
+          );
+        });
+        console.log(data)
+        setLikes(data);
+      });
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.listTitle}>Oma lista</Text>
       <View style={styles.listContainer}>
-        {props.likedPrograms.map(program => <ListItem key={program.id} program={program} />)}
+        {likes.length > 0 ? likes.map(program => <ListItem key={program.program_id} program={program.program_id} />) : null}
       </View>
       <StatusBar style="auto" />
     </View>
