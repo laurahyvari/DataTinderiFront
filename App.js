a31031a0f87db6b88d22961142964ad0455821ab
Sasu Korhonen <smage90@hotmail.com>
2021-04-28 20:20:42 +0300

    poistettu turha navigaatio ja tehty muutoksia korttiin

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
-import AuthNavigator from './navigation/AuthNavigator'
+import HomeScreen from './screens/HomeScreen'
 
 export default function App () {
-  return <AuthNavigator />
+  return <HomeScreen />
 }
